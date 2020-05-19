be7d7ff56a9a0046e81ba8f4232a49bd3ba0763c
Sowjanya Ennam <sowjanyaennam95@gmail.com>
2020-05-20 04:23:16 +0530

    Updates

---
+++ precache-manifest.11f590e3344970b3ecb98ce7407a9162.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90ff96bcd5bd21b1f60e0c36fe99c234",
+    "url": "/projects-tracking-app/index.html"
+  },
+  {
+    "revision": "08310c86e5fc55fc72eb",
+    "url": "/projects-tracking-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "dd7e3c3a589cc543b84b",
+    "url": "/projects-tracking-app/static/js/2.1940d80d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/projects-tracking-app/static/js/2.1940d80d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08310c86e5fc55fc72eb",
+    "url": "/projects-tracking-app/static/js/main.dfe7fc79.chunk.js"
+  },
+  {
+    "revision": "2434fc14bac268855318",
+    "url": "/projects-tracking-app/static/js/runtime-main.2cebc23b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/projects-tracking-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
